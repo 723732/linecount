1bcec8574cae6a818dd902495650baf6066c1b22
yxh <723732636@qq.com>
2019-03-30 11:46:52 +0800

    save

---
+++ CodeLine1/MainWindow.xaml.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CodeLine1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            _Model = new MyModel();
+            this.DataContext = _Model;
+        }
+        private MyModel _Model;
+
+        private void OpenFolder_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            _Model.FileName.Clear();
+            _Model.AllNum = 0;
+            _Model.WhiteNum = 0;
+            _Model.EffectNum = 0;
+            _Model.CommentNum = 0;
+            _Model.CommentRate = null;
+
+            var dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            try
+            {
+                if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    _Model.Path = dialog.FileName;
+                    
+                }
+                _Model.load_folder(dialog.FileName);
+                progressbar(_Model.FileName);
+
+                double result = (double)_Model.CommentNum / (double)_Model.AllNum;
+                _Model.CommentRate = string.Format("{0:0.0%}", result);//得到5.8%
+                MessageBox.Show("统计完毕");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void OpenFolder_CanExecuted(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void OpenFile_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            _Model.FileName.Clear();
+            _Model.AllNum = 0;
+            _Model.WhiteNum = 0;
+            _Model.EffectNum = 0;
+            _Model.CommentNum = 0;
+            _Model.CommentRate = null;
+
+            try
+            {
+                OpenFileDialog aDlg = new OpenFileDialog();
+                aDlg.Filter = "文本文件|*.txt;|所有文件|*.*";
+                if (aDlg.ShowDialog() != true) return;
+                //  MessageBox.Show(aDlg.FileName);
+                _Model.Path = aDlg.FileName;
+                _Model.load_file(aDlg.FileName);
+
+                progressbar(_Model.FileName);
+                
+                double result = (double)_Model.CommentNum / (double)_Model.AllNum;
+                _Model.CommentRate = string.Format("{0:0.0%}", result);//得到5.8%
+                MessageBox.Show("统计完毕");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void OpenFile_CanExecuted(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void progressbar(ObservableCollection<string> filename)//进度条
+        {
+            for (int i = 0; i < filename.Count(); i=i+3)
+            {
+
+                if(i+1< filename.Count())
+                {
+                    ParameterizedThreadStart s = new ParameterizedThreadStart(lineNum);
+                    Thread thread = new Thread(s);//创建一个线程
+                    thread.Start(i + 1);//开始一个线程
+                    thread.Join();
+                }
+
+                if (i + 2 < filename.Count())
+                {
+                    ParameterizedThreadStart a = new ParameterizedThreadStart(lineNum);
+                    Thread thread = new Thread(a);//创建一个线程
+                    thread.Start(i + 2);//开始一个线程
+                    thread.Join();
+                }
+
+                //if (filename[i].EndsWith(".v"))
+                //{
+                //    _Model.vlineCount(@filename[i]);
+                //}
+                //else if (filename[i].EndsWith(".vhd"))
+                //{
+                //    _Model.vhdlineCount(@filename[i]);
+                //}
+                lineNum(i);
+
+                double value = (i + 3) * 100.0 / filename.Count();
+                pbBar.Dispatcher.Invoke(new Action<System.Windows.DependencyProperty, object>(pbBar.SetValue), System.Windows.Threading.DispatcherPriority.Background, ProgressBar.ValueProperty, value);
+
+            }
+        }
+
+        private void lineNum(object arg)
+        {
+            int i= (int)arg;
+            if (_Model.FileName[i].EndsWith(".v"))
+            {
+                _Model.vlineCount(@_Model.FileName[i]);
+            }
+            else if (_Model.FileName[i].EndsWith(".vhd"))
+            {
+                _Model.vhdlineCount(@_Model.FileName[i]);
+            }
+        }
+    }
+}
---
+++ CodeLine1/MyModel.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CodeLine1
+{
+    class MyModel : INotifyPropertyChanged
+    {
+        public void load_folder(string aPath)//选择文件夹
+        {
+            DirectoryInfo dir = new DirectoryInfo(aPath);
+
+            FileInfo[] files = dir.GetFiles();//目录下文件
+            DirectoryInfo[] dii = dir.GetDirectories();//目录下子文件夹
+           
+            foreach (FileInfo info in files)
+            {
+                if (Regex.IsMatch(info.Name, @".*\.v$") || Regex.IsMatch(info.Name, @".*\.vhd$"))
+                {
+                    FileName.Add(info.FullName);
+                }
+            }
+
+            foreach (DirectoryInfo d in dii)
+            {
+                load_folder(d.FullName);
+            }
+            FileNum = FileName.Count();
+
+        }
+
+        public void load_file(string aPath)//选择文件
+        {
+            StreamReader sr = File.OpenText(aPath);
+            string s = "";
+
+            while ((s = sr.ReadLine()) != null)
+            {
+                s = s.Trim();
+
+                if (!s.StartsWith("//") && !Regex.IsMatch(s, @"^[\s]*$"))
+                {
+                    FileName.Add(s);
+                }
+            }
+            FileNum = FileName.Count();
+        }
+
+        public void vhdlineCount(string path)
+        {
+            StreamReader sr = File.OpenText(path);
+            string s = "";
+
+            while ((s = sr.ReadLine()) != null)
+            {
+                s = s.Trim();
+                AllNum++;
+
+                if (Regex.IsMatch(s, @"^[\s]*$"))//匹配空白行
+                {
+                    WhiteNum++;
+                }
+                else if (s.StartsWith("--"))
+                {
+                    CommentNum++;
+                }
+                else
+                    EffectNum++;
+            }
+            
+        }
+
+        public void vlineCount(string path)
+        {
+            StreamReader sr = File.OpenText(path);
+            string s = "";
+
+            while ((s = sr.ReadLine()) != null)
+            {
+                s = s.Trim();
+                AllNum++;
+
+                if (Regex.IsMatch(s, @"^[\s]*$"))//匹配空白行
+                {
+                    WhiteNum++;
+                }
+                else if (s.StartsWith("//"))
+                {
+                    CommentNum++;
+                }
+                else if (s.StartsWith("/*") && s.EndsWith("*/"))
+                {
+                    CommentNum++;
+                }
+                else if (s.StartsWith("/*") && !s.EndsWith("*/"))
+                {
+                    CommentNum++;
+                    comment = true;
+                }
+                else if (comment == true)
+                {
+                    CommentNum++;
+                    if (s.EndsWith("*/"))
+                        comment = false;
+                }
+                else
+                    EffectNum++;
+            }
+          //  CommentRate = (double)CommentNum / (double)AllNum;
+        }
+
+        private string _Path;
+        private ObservableCollection<string> _FileName = new ObservableCollection<string>();
+        private long _AllNum;
+        private long _WhiteNum;
+        private long _EffectNum;
+        private long _CommentNum;
+        private string _CommentRate;
+        private bool comment = false;
+        private long _FileNum;
+
+        public string Path
+        {
+            get { return _Path; }
+            set
+            {
+                _Path = value;
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("Path"));
+            }
+        }
+
+        public long AllNum
+        {
+            get { return _AllNum; }
+            set
+            {
+                _AllNum = value;
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("AllNum"));
+            }
+        }
+
+        public long WhiteNum
+        {
+            get { return _WhiteNum; }
+            set
+            {
+                _WhiteNum = value;
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("WhiteNum"));
+            }
+        }
+
+        public long EffectNum
+        {
+            get { return _EffectNum; }
+            set
+            {
+                _EffectNum = value;
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("EffectNum"));
+            }
+        }
+
+        public long CommentNum
+        {
+            get { return _CommentNum; }
+            set
+            {
+                _CommentNum = value;
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("CommentNum"));
+            }
+        }
+
+        public string CommentRate
+        {
+            get { return _CommentRate; }
+            set
+            {
+                _CommentRate = value;
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("CommentRate"));
+            }
+        }
+
+        public ObservableCollection<string> FileName
+        {
+            get { return _FileName; }
+            set
+            {
+                _FileName = value;
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("FileName"));
+            }
+        }
+
+        public long FileNum
+        {
+            get { return _FileNum; }
+            set
+            {
+                _FileNum = value;
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("FileNum"));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
